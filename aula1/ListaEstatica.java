ff29efdc2537e497b0a0d8c7983c05e1a800c6e6
Maria Eduarda <ouriquemariaeduarda@gmail.com>
2025-02-26 18:29:52 -0300

    códigos aula 1

---
+++ aula1/ListaEstatica.java
@@ -0,0 +1,27 @@
+package aula1;
+public class ListaEstatica {
+
+    private String[] elementos;
+    private int tamanho;
+    private int CAPACIDADE;
+
+    public ListaEstatica() {
+        CAPACIDADE = 10;
+        elementos = new String[CAPACIDADE];
+        this.tamanho = 0;
+    }
+
+    public void adicionar(String elemento) {
+        elementos[tamanho] = elemento;
+        tamanho++;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < tamanho; i++) {
+            sb.append(elementos[i]).append(" ");
+        }
+        return sb.toString();
+    }
+}
---
+++ aula1/Revisao.java
@@ -0,0 +1,15 @@
+package aula1;
+
+public class Revisao {
+
+    public static void main(String[] args) {
+        ListaEstatica minhaLista = new ListaEstatica();
+        minhaLista.adicionar("bernardo");
+        minhaLista.adicionar("gabriel");
+        minhaLista.adicionar("salete");
+        System.out.println(minhaLista);
+
+    }
+
+
+}
